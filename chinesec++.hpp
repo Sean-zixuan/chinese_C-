b2924cd1196e57edc022c679c85ac3a35695dbec
Sean_Finding <68492025+Sean-zixuan@users.noreply.github.com>
2022-05-01 18:50:02 +0800

    Create chinesec++.hpp

---
+++ chinesec++.hpp
@@ -0,0 +1,62 @@
+//
+//  chinesec++.hpp
+//  Chinese C++
+//
+//  Created by Sean  on 2022/5/1.
+//
+
+#ifndef chinesec___hpp
+#define chinesec___hpp
+#include <stdio.h>
+//======================
+#define 主函数 main
+#define 整数类型的 int
+#define 长整数类型的 long long
+#define 小数类型的 double
+#define 浮点数类型的 float
+#define 字符类型的 char
+#define 字符串类型的 string
+#define 布尔类型的 bool
+#define 定义整数类型的 int
+#define 定义长整数类型的 long long
+#define 定义小数类型的 double
+#define 定义浮点数类型的 float
+#define 定义字符类型的 char
+#define 定义字符串类型的 string
+#define 定义布尔类型的 bool
+#define 定义未知类型的 void
+#define 返回 return
+#define 等于 =
+#define 为 =
+#define 如果 if
+#define 大于 >
+#define 小于 <
+#define 大于等于 >=
+#define 小于等于 <=
+#define 循环 for
+#define 输入 cin
+#define 输出 cout
+#define 导入 #include
+#define 定义常量为 #define
+#define 声明常量为 const
+#define 换行 endl
+#define 定义结构体 strcut
+#define 排序 sort
+#define 加 +
+#define 减 -
+#define 乘 *
+#define 除 /
+#define 求余 %
+#define 不等于 !=
+#define 并且 &&
+#define 或者 ||
+#define 循环至 while(
+#define 为否 )
+#define 累加 ++
+#define 累减 --
+#define 左移 <<
+#define 右移 >>
+#define 的第 [
+#define 项 ]
+//======================
+#endif /* chinesec___hpp */
